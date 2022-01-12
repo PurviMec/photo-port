ce6a99245c04aee07f8a9846b4870766be295c1a
PurviMec <princy.mecwan94@gmail.com>
2022-01-12 14:13:39 -0500

    create Model component and modify it to render when needed

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -60,7 +60,7 @@ function ContactForm() {
                         <p className="error-text">{errorMessage}</p>
                     </div>
                 )}
-                <button data-testid="button" type="submit">Submit</button>
+                <button data-testid="button" wtype="submit">Submit</button>
             </form>
         </section>
     );
